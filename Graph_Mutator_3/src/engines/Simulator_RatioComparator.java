c7494007caea35b6e671fb961db8463243698917
jtony123 <jtony123@gmail.com>
2016-03-21 16:34:53 +0000

    Ratio Comaparator class added

---
+++ Graph_Mutator_3/src/engines/Simulator_RatioComparator.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package engines;
+
+/**
+ * @author Anthony Jackson
+ * @id 11170365
+ *
+ */
+public class Simulator_RatioComparator {
+
+	/**
+	 * 
+	 */
+	public Simulator_RatioComparator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
